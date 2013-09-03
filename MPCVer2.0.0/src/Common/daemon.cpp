133560bc7c90bcb7058039e0a49f16a4c025e905
zhiguangzhou <zhouklansman@gmail.com>
2013-09-03 10:52:55 +0800

    windows 服务基础类

---
+++ MPCVer2.0.0/src/Common/daemon.cpp
@@ -0,0 +1,54 @@
+#include "daemon.h"
+#include <stddef.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <Windows.h> 
+
+extern const char *g_genie_server_name;
+
+TestCon2::TestCon2()
+{
+	m_daemon = GenieDaemon::create();
+}
+
+TestCon2::~TestCon2()
+{
+	delete m_daemon;
+}
+
+int TestCon2::run()
+{
+	if (!m_daemon->start()) {
+		return -1;
+	}
+
+	for (;;) {
+		std::string cmd;
+		std::cin >> cmd;
+
+		if (!std::cin) {
+			break;
+		}
+
+		if (cmd == "quit") {
+			break;
+		} else if (cmd == "restart") {
+		} else if (cmd == "stop") {
+		} else if (cmd == "start") {
+		} else if (cmd == "arp") {
+		} else if (cmd == "netstat") {
+		}
+	}
+
+	m_daemon->stop();
+	return 0;
+}
+
+int console_main(int argc, char **argv)
+{
+	TestCon2 con;
+	return con.run();
+}
+
